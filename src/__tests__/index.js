90ddfcace29807b58c6b7f36ac878c4ab4f52551
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2020-02-12 09:28:57 -0800

    update deps

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,16 +1,25 @@
-import {createDeployment, getPendingDeployments, createDeploymentStatus} from '..';
+import {
+  createDeployment,
+  getPendingDeployments,
+  createDeploymentStatus,
+} from '..';
 
-jest.mock('@octokit/rest', () => class Octokit {
+jest.mock('@octokit/rest', () => ({
+  Octokit: class {
   // eslint-disable-next-line require-jsdoc
-  get repos() {
-    return {
-      createDeployment: () => Promise.resolve({data: {id: 'foo'}}),
-      createDeploymentStatus: () => Promise.resolve({data: {}}),
-      listDeployments: () => Promise.resolve({data: [{id: 'foo'}]}),
-      listDeploymentStatuses: () => Promise.resolve({data: [{state: 'success', updated_at: '2019-11-07T06:06:31Z'}]}),
-    };
-  }
-});
+    get repos() {
+      return {
+        createDeployment: () => Promise.resolve({data: {id: 'foo'}}),
+        createDeploymentStatus: () => Promise.resolve({data: {}}),
+        listDeployments: () => Promise.resolve({data: [{id: 'foo'}]}),
+        listDeploymentStatuses: () =>
+          Promise.resolve({
+            data: [{state: 'success', updated_at: '2019-11-07T06:06:31Z'}],
+          }),
+      };
+    }
+  },
+}));
 
 describe('Utility Functions', () => {
   test('createDeployment returns a promise', () => {
